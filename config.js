d25380001131d8160c1f11f767c424aba1864703
Chandra Wijaya <chandrapinter25@gmail.com>
2026-06-18 22:14:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,6 +8,6 @@ window.GYM_CONFIG = {
   SCRIPT_URL:"https://script.google.com/macros/s/AKfycbzLkwBRyKEzWsDwBACd2eBPxfXopAD_Jh1x3PIJ8fyc1SX0NN3T6pbxZBzNk4zPaSOT/exec",
   APP_NAME: "Sistem Admin Gym",
   GYM_NAME: "SISTEM FC",
-  REFRESH_INTERVAL_MS: 10000,
+  REFRESH_INTERVAL_MS: 20000,
   MAX_KEYS: 100
 };
